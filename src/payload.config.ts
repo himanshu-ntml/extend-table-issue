79e1da380ea9d2ac779a7c4d7fb35a4e66bc4922
Himanshu Ahuja <himanshu@nicolethemathlady.com>
2024-12-20 19:44:59 +0530

    first commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,9 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
+import { index, integer } from '@payloadcms/db-postgres/drizzle/pg-core'
+
+
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 
@@ -20,7 +23,19 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, {
+    slug: 'places',
+    fields: [
+      {
+        name: 'country',
+        type: 'text',
+      },
+      {
+        name: 'city',
+        type: 'text',
+      },
+    ],
+  },],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -30,6 +45,23 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    afterSchemaInit: [
+      ({ schema, extendTable, adapter }) => {
+        extendTable({
+          table: schema.tables.places,
+          columns: {
+            extraIntegerColumn: integer('extra_integer_column'),
+          },
+          extraConfig: (table) => ({
+            country_city_composite_index: index('country_city_composite_index').on(
+              table.country,
+              table.city,
+            ),
+          }),
+        })
+        return schema
+      },
+    ],
   }),
   sharp,
   plugins: [
